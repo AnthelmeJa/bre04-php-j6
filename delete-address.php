9ec340c1ffbbe7731dad7f4a43abe98444ef9b25
AnthelmeJa <anthelme.jarreau35@gmail.com>
2025-06-11 13:59:33 +0000

    etape 8 : delete  address

---
+++ delete-address.php
@@ -0,0 +1,13 @@
+<?php
+
+    require "connexion.php";
+    
+    $query = $db->prepare("DELETE FROM address WHERE id = :id");
+    
+    $parameters = [
+        'id' => $_POST['id']
+    ];
+    
+    $query->execute($parameters);
+
+?>
